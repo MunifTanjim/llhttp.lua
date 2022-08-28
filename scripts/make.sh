808fd185a8b37c9d9f9f7624ae94eb16220eb951
Munif Tanjim <hello@muniftanjim.dev>
2022-08-28 23:14:00 +0600

    chore: add scripts/make.sh

---
+++ scripts/make.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+declare ROOT_DIR
+ROOT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+
+source "${ROOT_DIR}/_helper.sh"
+
+declare rockspec_version version
+rockspec_version="$(get_rockspec_version "${1:-}")"
+version="$(get_version "${rockspec_version}")"
+
+declare rockspec
+rockspec="$(get_rockspec "${rockspec_version}")"
+
+if [[ "${version}" = "dev" ]]; then
+  "${ROOT_DIR}/generate-rockspec.sh" "${rockspec_version}"
+fi
+
+echo "generating compile_flags.txt..."
+echo "-Illhttp" > compile_flags.txt
+echo "-I$(luarocks config variables.LUA_INCDIR)" >> compile_flags.txt
+echo
+
+echo "luarocks make..."
+luarocks make --no-install "${rockspec}"
